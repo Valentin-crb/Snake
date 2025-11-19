c4938890a3ef72fd6758fbff05cb303c491e06ab
Valentin-crb <curbat.valentin@gmail.com>
2025-11-19 10:52:04 +0200

    am facut sa se deschida un ecran negru

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("Snake")
+clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
+    clock.tick(60)
+    
