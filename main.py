935c1dbcbc3696f5804580f833c0a9f828e459eb
Valentin-crb <curbat.valentin@gmail.com>
2025-12-13 14:46:23 +0200

    am facut sa se miste capul

--- main.py
+++ main.py
@@ -15,23 +15,36 @@ x_head = 400
 y_head = 300
 head = pygame.Rect(x_head, y_head, 20, 20)
 culoare_sarpe = (0,204,0)
+directie = ""
 
 while True:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w and directie != "jos":
+                directie = "sus"
+            if event.key == pygame.K_s and directie != "sus":
+                directie = "jos"
+            if event.key == pygame.K_a and directie != "dreapta":
+                directie = "stanga"
+            if event.key == pygame.K_d and directie != "stanga":
+                directie = "dreapta"
     
+    
+    if directie == "sus":
+        head.y -=20
+    if directie == "jos":
+        head.y +=20
+    if directie == "stanga":
+        head.x -=20
+    if directie == "dreapta":
+        head.x +=20
     screen.fill((204,204,0))
-    #umplu ecranul cu patratele
-    for x in range(0,SCREEN_WIDTH,20):
-        for y in range(0,SCREEN_HEIGHT,20):
-            pygame.draw.rect(screen,(204,204,0), (x,y, 20,20))
 
-    
     pygame.draw.rect(screen, culoare_sarpe, head, border_radius= 5)
-    for contor in range(20,100):
-        pygame.draw.rect(screen,culoare_sarpe, (x_head, y_head+contor, 20, 20), border_radius=5)
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(5)
     
