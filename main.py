11ffeb7c4e1097a2eafb114a4c543750968009df
Valentin-crb <curbat.valentin@gmail.com>
2025-12-15 17:01:55 +0200

    restart joc + fixare buguri

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ clock = pygame.time.Clock()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-GAME_WIDTH = 700
-GAME_HEIGHT = 500
+GAME_WIDTH = 680
+GAME_HEIGHT = 480
 x_head = 400
 y_head = 300
 game_over = False
 game_start = False
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-game_screen = pygame.Rect(50,50,GAME_WIDTH, GAME_HEIGHT)
+game_screen = pygame.Rect(60,60,GAME_WIDTH, GAME_HEIGHT)
 culoare_sarpe = (0,204,0)
+culoare_joc = (102, 102, 51)
 directie = ""
 sarpe = [pygame.Rect(x_head, y_head, 20, 20),
          pygame.Rect(x_head, y_head+20, 20, 20),
@@ -29,8 +30,11 @@ culoare_mar =(230,0,0)
 font = pygame.font.SysFont(None, 48)
 text_game_over = font.render("GAME OVER", True, (255, 0, 0))
 text_game_start = font.render("PRESS W TO START", True, culoare_sarpe)
+text_score = font.render("SCORE: ",True, culoare_joc)
 rect_over = text_game_over.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
-rect_start = text_game_start.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT//4))
+rect_start = text_game_start.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT//2))
+rect_score = text_score.get_rect(bottomleft=(game_screen.left, game_screen.top - 5))
+
 
 
 while True:
@@ -53,11 +57,18 @@ while True:
                 directie = "stanga"
             if event.key == pygame.K_d and directie != "stanga":
                 directie = "dreapta"
-            if event.key == pygame.K_r:
+            if event.key == pygame.K_r and game_over: #restart joc
                 game_over = False
+                sarpe = [pygame.Rect(x_head, y_head, 20, 20),
+                pygame.Rect(x_head, y_head+20, 20, 20),
+                pygame.Rect(x_head, y_head+40, 20, 20),
+                pygame.Rect(x_head, y_head+60, 20, 20),
+                pygame.Rect(x_head, y_head+80, 20, 20)]
+                directie = "sus"
     if not game_over and game_start:
         screen.fill((204,204,0))
-        pygame.draw.rect(screen, (102, 102, 51),game_screen,20)
+        pygame.draw.rect(screen, culoare_joc,game_screen) #desenare ecran
+        screen.blit(text_score, rect_score)
 
         pozitii_vechi = [(seg.x, seg.y) for seg in sarpe]
 
@@ -70,9 +81,12 @@ while True:
         elif directie == "dreapta":
             sarpe[0].x +=20
 
-        if sarpe[0].colliderect(mar):
-            mar.x = random.randrange(100,GAME_WIDTH,20)
-            mar.y = random.randrange(100,GAME_HEIGHT,20)
+        if sarpe[0].colliderect(mar): #redesenare mar
+            while True:
+                mar.x = random.randrange(game_screen.left, game_screen.right, 20)
+                mar.y = random.randrange(game_screen.top, game_screen.bottom, 20)
+                if not any(mar.colliderect(seg) for seg in sarpe):
+                    break
             x,y = pozitii_vechi[-1]
             bloc_nou = pygame.Rect(x,y,20,20)
             sarpe.append(bloc_nou)
@@ -86,7 +100,7 @@ while True:
         pygame.draw.rect(screen,culoare_mar,mar)
 
         for i in range(2, len(sarpe)):
-            if sarpe[0].colliderect(sarpe[i]) or sarpe[0].collide:
+            if sarpe[0].colliderect(sarpe[i]) or not game_screen.contains(sarpe[0]):
                 game_over = True
         
     if game_over:
